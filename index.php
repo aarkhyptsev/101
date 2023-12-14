5fe4ec5899dd4d3092f7d5b9233fb444bd6bde86
Andrii <aarkhyptsev@gmail.com>
2023-12-14 20:29:00 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+require('classes/Posts.php');
+
+
+
+use App\Posts;
+$title = 'My Blog';
+
+$posts = Posts::getAll();
+$numPosts = count($posts);
+$postText = Posts::getText($numPosts);
+$numPostsDisplay = "$numPosts $postText";
+?>
+    <h1><?= $title ?></h1>
+    <h2><?= $numPostsDisplay ?></h2>
+<?php for ($i = 0; $i < $numPosts; $i++) : ?>
+    <h3><?= $posts[$i]['title'] ?></h3>
+    <p><?= $posts[$i]['content'] ?></p>
+    <p>By <?= $posts[$i]['author'] ?></p>
+<?php endfor ?>
